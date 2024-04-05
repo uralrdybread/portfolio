5687b7cf7bdb174ff9e75255afa736f2ff8c9f71
Douglas White <douglas.rw@outlook.com>
2024-04-04 18:29:25 -0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<x-layout>
+
+    <div class="grid grid-cols-3 gap-4">
+        <div class="relative w-full max-w-300" id="nuke">
+            <img class="object-cover w-full h-full" src="images/nucleardeckchairs54.png" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+        <div class="relative w-full max-w-300" id="super">
+            <img class="object-cover w-full h-full" src="images/sstandsfor54.png" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+        <div class="relative w-full max-w-300" id="toshiro">
+            <img class="object-cover w-full h-full" src="images/toshiro11.webp" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+        <div class="relative w-full max-w-300" id="green">
+            <img class="object-cover w-full h-full" src="images/greenlantern47.png" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+        <div class="relative w-full max-w-300" id="snow">
+            <img class="object-cover w-full h-full" src="images/snowysteps23.png" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+        <div class="relative w-full max-w-300" id="espresso">
+            <img class="object-cover w-full h-full" src="images/espresso45.png" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+        <div class="relative w-full max-w-300" id="bomber">
+            <img class="object-cover w-full h-full" src="images/bomber80s11.webp" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+        <div class="relative w-full max-w-300" id="krypton">
+            <img class="object-cover w-full h-full" src="images/echosofkrypton74.png" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+        <div class="relative w-full max-w-300" id="adam">
+            <img class="object-cover w-full h-full" src="images/adamnjensen45.png" alt="nuclear explosion being watched by 1950s">
+            <p class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-lg font-bold tracking-wide bg-white bg-opacity-10 opacity-0 hover:opacity-100 cursor-default hover:cursor-pointer transition-all duration-200 ease-in-out">View Project</p>
+        </div>
+    </div>
+
+</x-layout>
