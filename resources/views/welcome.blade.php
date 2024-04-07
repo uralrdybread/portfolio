f0328ef159dfaa51334da87c4fd62b81f2d142ff
Douglas White <douglas.rw@outlook.com>
2024-04-06 21:07:21 -0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 <x-layout>
 
 
-
 <header class="py-10 px-3 md:px-10 fixed top-0 left-0 z-30 bg-customBlue w-full">
   <div class="max-w-6xl mx-auto">
     <div class="flex justify-between">
@@ -10,6 +9,7 @@
         <!-- Hidden badge on small screens -->
         <span class="mx-3 hidden md:inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">AI Artist</span>
       </div>
+      <!-- Hamburger Menu Icon -->
     <nav class="right space-x-5 items-center flex">
         <a href="#work" class="hover:underline hidden md:flex">Work</a>
         <a href="#contact" class="hover:underline hidden md:flex">Contact</a>
@@ -22,17 +22,15 @@
                 <a href="#x">
             <div class="text-3xl"><i class="bi bi-twitter-x hidden md:flex"></i></div>
                 </a>
-            <div class="md:hidden flex flex-col justify-center">
-                <div class="bar bg-black h-3 w-24 m-1 rounded-2xl"></div>
-                <div class="bar bg-black h-3 w-24 m-1 rounded-2xl"></div> 
-                <div class="bar bg-black h-3 w-24 m-1 rounded-2xl"></div> 
-            </div>
+<div class="md:hidden flex flex-col justify-center">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-14 h-14">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>
+  </div>
     </nav>
         </div>
   </div>
 </header>
-
-
 
 
 <div class=" bg-customBlue pt-24">
